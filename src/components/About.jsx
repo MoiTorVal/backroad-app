0f6f3ada72b0b921f1df113276487306bb6f3744
moises valdez <shmoi@moisess-MacBook-Pro.local>
2024-01-12 08:58:53 -0800

    uncommented a css code to show image on the hero page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import aboutImg from "../images/about.jpeg";
+
 const About = () => {
   return (
     <section class="section" id="about">
@@ -11,11 +13,7 @@ const About = () => {
 
       <div class="section-center about-center">
         <div class="about-img">
-          <img
-            src="./images/about.jpeg"
-            class="about-photo"
-            alt="awesome beach"
-          />
+          <img src={aboutImg} class="about-photo" alt="awesome beach" />
         </div>
         <article class="about-info">
           <h3>explore the difference</h3>
